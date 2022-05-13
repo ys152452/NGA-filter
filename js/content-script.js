bfb9d2fbfe265d2104057fc7d7b02bce77dd3143
ys152452 <362565210@qq.com>
2022-05-13 19:47:37 +0800

    开工

---
+++ js/content-script.js
@@ -0,0 +1,23 @@
+(function () {
+  let { search } = location;
+  let key = String("tid");
+  const today = new Date().getTime();
+  if (search.indexOf(key) > -1) {
+    let outArr = document.getElementsByClassName("forumbox");
+    for (let i = 0; i < outArr.length; i++) {
+      let arr = outArr[i].getElementsByClassName("numeric");
+      if (arr.length) {
+        for (let j = 0; j < arr.length; j++) {
+          let name = arr[j].getAttribute("name");
+          if (name == "regdate") {
+            let curReg = new Date(`20${arr[j].innerHTML}`).getTime();
+            console.log(today - curReg);
+            if (today - curReg < 7879680000) {
+              outArr[i].style.display = "none";
+            }
+          }
+        }
+      }
+    }
+  }
+})();
